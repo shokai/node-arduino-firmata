9062f586530ac6597c220f56fdbd4e1e4459c236
Sho Hashimoto <hashimoto@shokai.org>
2014-01-26 18:15:29 +0900

    js sample

---
+++ samples/led_blink.js
@@ -0,0 +1,16 @@
+var ArduinoFirmata = require(__dirname+'/../');
+// var ArduinoFirmata = require('arduino-firmata');
+
+var arduino = new ArduinoFirmata().connect();
+
+arduino.on('connect', function(){
+  console.log("connect!! "+arduino.serialport_name);
+
+  var stat = true
+  setInterval(function(){
+    console.log(stat);
+    arduino.digitalWrite(13, stat);
+    arduino.digitalWrite(12, !stat);
+    stat = !stat;  // blink
+  }, 300);
+});
